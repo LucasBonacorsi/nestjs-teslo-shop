b9930b16470828816f199e58300950b61a3eb39a
Lucas Bonacorsi <lucasbonacorsi@hotmail.com>
2023-02-11 11:26:21 -0300

    first commit

---
+++ src/files/files.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
+  uploadProductImage(
+    @UploadedFile() file: Express.Multer.File,
+  ): Express.Multer.File {
+    console.log({ fileController: file });
+
+    return file;
+  }
+}
